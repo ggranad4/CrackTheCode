d34fd6f904250da295ed1a3bcc52063fa33cd6df
ggranad4 <gerardogranados45@yahoo.com>
2020-07-21 22:51:30 -0700

    added functions to linkedList

--- LinkedList/src/Driver.java
+++ LinkedList/src/Driver.java
@@ -1,21 +1,11 @@
 
 public class Driver {
-	public static void main(String []args) {
-		Node linkedList = new Node(2);
-		linkedList.appendToTail(1);
-		linkedList.appendToTail(1);
-		linkedList.appendToTail(1);
-		linkedList.appendToTail(3);
-		linkedList.appendToTail(4);
-		linkedList.appendToTail(4);
-		linkedList.appendToTail(111);
-		linkedList.appendToTail(111);
-		linkedList.appendToTail(10);
-		linkedList.appendToTail(10);
+	public static void main(String []args) throws Exception {
+		Node linkedList1 = new Node(7);
+		linkedList1.appendToTail(3);
+		linkedList1.appendToTail(7);
 		
-		linkedList = linkedList.deleteDuplicates(linkedList);
-		
-		linkedList.showList();
+		Node linkedList2 = new Node(5);
 		
 	}
 
--- LinkedList/src/Node.java
+++ LinkedList/src/Node.java
@@ -6,6 +6,7 @@ public class Node {
 	public Node (int d) {
 		data = d;
 	}
+	
 	void appendToTail(int d) {
 		Node end = new Node(d);
 		Node n = this;
@@ -46,8 +47,8 @@ public class Node {
 			n = n.next;
 		}
 		return head;
-		
 	}
+	
 	void showList() {
 		Node n = this;
 		while(n.next != null) {
@@ -56,8 +57,110 @@ public class Node {
 		
 		}
 		System.out.println(n.data);
+	}
+	
+	void kthElementToLast(int findKth) {
+		Node n = this;
+		while (n.next != null) {
+			if(n.data == findKth) {
+				System.out.print("Kth Element = ");
+				while(n.next.next != null) {
+					System.out.print(n.data + " --> ");
+					n = n.next;
+				}
+				System.out.print(n.data + " --> ");
+			}
+			n = n.next;
+			
+		}
+		System.out.print( n.data );
 		
 	}
-
 	
+	void deleteNode(int deleteNode) {
+		Node n = this;
+		Node previous = null;
+		
+		while(n.next != null) {
+			
+			if(n.data == deleteNode) {
+				previous.next = n.next;
+				n = previous;
+			}
+			previous = n;
+			n = n.next;
+		}
+	}
+	
+	void partition(int integerSetter) {
+		Node n = this;
+		ArrayList<Integer> listOfIntegersBelow = new ArrayList<>();
+		
+		ArrayList<Integer> listOfIntegersAbove = new ArrayList<>();
+		
+		while(n != null) {
+			if(integerSetter > n.data) {
+				listOfIntegersBelow.add(n.data);
+			}
+			else {
+				listOfIntegersAbove.add(n.data);
+			}
+			n = n.next;
+		}
+		for(Integer number: listOfIntegersBelow) {
+			System.out.print(number + " --> ");
+		}
+		System.out.print("        -->         ");
+		for(Integer number: listOfIntegersAbove) {
+			System.out.print(number + " --> ");
+			
+		}
+		
+	}
+	
+	int addLinkedLists(Node linkedList1, Node linkedList2) throws Exception {
+		Node n1 = linkedList1;
+		Node n2 = linkedList2;
+				
+		if(linkedList1 == null || linkedList2 == null) {
+			throw new Exception("Cannot have empty linkedList");
+		}
+		StringBuilder num1 = new StringBuilder();
+		StringBuilder num2 = new StringBuilder();
+		
+		while(n1 != null) {
+			num1.append(n1.data);
+			n1 = n1.next;
+		}
+		while(n2 != null) {
+			num2.append(n2.data);
+			n2 = n2.next;
+		}
+		
+		int numberOne = Integer.valueOf(num1.reverse().toString());
+		int numberTwo = Integer.valueOf(num2.reverse().toString());
+		
+		return numberOne + numberTwo;
+		}
+
+	  
+	boolean isPalindrome() {
+		Node n = this;
+		StringBuilder sb = new StringBuilder();
+		
+		while(n != null) {
+			sb.append(n.data);
+			n = n.next;
+		}
+		String normal = sb.toString();
+		String reverse = sb.reverse().toString();
+		System.out.println(normal + " " + reverse);
+		if(normal.equals(reverse)){
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
 }
